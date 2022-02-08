b35a0aaeb893dd98a6524c0c440a32f914708335
Omolola Aremu <aremuomolola7@gmail.com>
2022-02-08 11:59:12 +0100

    Add project files.

---
+++ UserDb/dbo/StoredProcedures/spUser_getAll.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[spUser_getAll]
+	AS
+	begin
+		select Id, FirstName, LastName
+		from dbo.[User];
+	end
---
+++ UserDb/dbo/StoredProcedures/spUsers_get.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spUsers_get]
+	@Id int
+	AS
+	begin
+	select Id, FirstName, LastName
+		from dbo.[User]
+		where Id=@Id;
+
+	end
